a5d2ad9a407e7ee3abceb0825de7c3b3caeb36ff
hellomac87 <hellomac87@gmail.com>
2019-01-10 12:11:00 +0900

    step2. 기본적인 webpack 설정 파일 만들기

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import '../css/index.css';
-const title = 'Webpack Configration';
+const title = 'Webpack Configuration';
 const $el = document.getElementById('root');
 
-$el.innerHTML = `<h1>Hello World ${title}</h1>`
+$el.innerHTML = `<h1>Hello World! ${title}</h1>`;
